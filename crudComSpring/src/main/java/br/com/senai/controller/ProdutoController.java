fc6560fbd12bf58405073ae0541dab5b7d91a9f5
luan <luan@gmail.com>
2023-06-16 13:29:45 -0300

    alterações

--- crudComSpring/src/main/java/br/com/senai/controller/ProdutoController.java
+++ crudComSpring/src/main/java/br/com/senai/controller/ProdutoController.java
@@ -2,7 +2,12 @@ package br.com.senai.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +26,20 @@ public class ProdutoController {
 	@GetMapping
 	public List<Produto> listaProdutos() {
 		return produtorepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> findById(@PathVariable Long id){
+		return produtorepository.findById(id)
+				.map(gravado -> ResponseEntity.ok().body(gravado))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	public ResponseEntity<Produto> cadastrarProduto(
+			@RequestBody Produto produto){
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(produtorepository.save(produto));
 	}
 
 }
